6230e93671bfc0014fa74016e0475aa5e638079b
hahoanghai <hahoanghai.ueh@gmail.com>
2016-01-06 10:49:11 -0800

    1st try it

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+	echo "hello world";
+	echo "<br>";
+
+	$temp = "Jim";
+	echo " hi, my name is ";
+	echo $temp;
+	$temp = 'geek';
+	echo " I am a ";
+	echo $temp;
+	$temp = 10;
+	echo " My level is ";
+	echo $temp;
+
+?>
