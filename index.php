84f3dfefc522f503fdd7514a5825c146913e9785
hahoanghai <hahoanghai.ueh@gmail.com>
2016-01-06 10:59:38 -0800

    2 try it

--- index.php
+++ index.php
@@ -1,15 +1,9 @@
 <?php
-	echo "hello world";
-	echo "<br>";
-
-	$temp = "Jim";
-	echo " hi, my name is ";
-	echo $temp;
-	$temp = 'geek';
-	echo " I am a ";
-	echo $temp;
-	$temp = 10;
-	echo " My level is ";
-	echo $temp;
-
+	$name = "Jim";
+	$what = "geek";
+	$level = 10;
+	echo "Hi, my name is ".
+		$name.
+		" , and I am a level ".
+		$level . " " . $what;
 ?>
