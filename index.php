843e60290f0ed4cd1fdf1be2e4659215f8cbf536
hahoanghai <hahoanghai.ueh@gmail.com>
2016-01-09 12:52:53 -0800

    7 try it

--- index.php
+++ index.php
@@ -11,29 +11,16 @@
 
 	function winner($token,$position) {
 		$won = false;
-		if (($position[0] == $token) && ($position[1] == $token) && ($position[2] == $token)) {
+
+		for($row=0; $row<3; $row++) {
 			$won = true;
-		} 
-		else if (($position[3] == $token) && ($position[4] == $token) && ($position[5] == $token)) {
-			$won = true;
-		}
-		else if (($position[6] == $token) && ($position[7] == $token) && ($position[8] == $token)) {
-			$won = true;
-		}
-		else if (($position[0] == $token) && ($position[4] == $token) && ($position[8] == $token)) {
-			$won = true;
-		}
-		else if (($position[0] == $token) && ($position[3] == $token) && ($position[6] == $token)) {
-			$won = true;
-		}
-		else if (($position[1] == $token) && ($position[4] == $token) && ($position[7] == $token)) {
-			$won = true;
-		}
-		else if (($position[2] == $token) && ($position[5] == $token) && ($position[8] == $token)) {
-			$won = true;
-		}
-		else if (($position[2] == $token) && ($position[4] == $token) && ($position[6] == $token)) {
-			$won = true;
+			for($col=0; $col<3; $col++){
+				if ($position[3*$row+$col] != $token) {
+					$won = false; 
+				}
+			}
+			if($won)
+				return $won;
 		}
 	return $won;
 	}
