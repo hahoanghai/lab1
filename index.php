3fbb93423e42ecf5080bd241767ca558e2fa7140
hahoanghai <hahoanghai.ueh@gmail.com>
2016-01-09 00:05:35 -0800

    6 try it

--- index.php
+++ index.php
@@ -1,20 +1,40 @@
 <?php
-	$name = "Jim";
-	$what = "geek";
-	$level = 10;
-	echo "Hi, my name is ".
-		$name.
-		" , and I am a level ".
-		$level . " " . $what;
+	$position = $_GET['board'];
+	$squares = str_split($position);
 
-	echo "<br/>";
+	if (winner('x',$squares)) 
+		echo 'X win.';
+	else if (winner('o',$squares)) 
+		echo 'O win.';
+	else 
+		echo 'No winner yet.';
 
-	$hoursworked = $_GET['hours'];
-	$rate = 12;
-	if($hoursworked > 40)
-		$total = $hoursworked * $rate * 1.5;
-	else
-		$total = $hoursworked * $rate;
-
-	echo ($total > 0) ? 'You owe me ' . $total : "You're welcome";
+	function winner($token,$position) {
+		$won = false;
+		if (($position[0] == $token) && ($position[1] == $token) && ($position[2] == $token)) {
+			$won = true;
+		} 
+		else if (($position[3] == $token) && ($position[4] == $token) && ($position[5] == $token)) {
+			$won = true;
+		}
+		else if (($position[6] == $token) && ($position[7] == $token) && ($position[8] == $token)) {
+			$won = true;
+		}
+		else if (($position[0] == $token) && ($position[4] == $token) && ($position[8] == $token)) {
+			$won = true;
+		}
+		else if (($position[0] == $token) && ($position[3] == $token) && ($position[6] == $token)) {
+			$won = true;
+		}
+		else if (($position[1] == $token) && ($position[4] == $token) && ($position[7] == $token)) {
+			$won = true;
+		}
+		else if (($position[2] == $token) && ($position[5] == $token) && ($position[8] == $token)) {
+			$won = true;
+		}
+		else if (($position[2] == $token) && ($position[4] == $token) && ($position[6] == $token)) {
+			$won = true;
+		}
+	return $won;
+	}
 ?>
